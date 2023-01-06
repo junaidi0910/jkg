3aa98d5b6ffa836ce58dd38bc6442ea61a686960
evanleonardo <evan.leonardo@si.ukdw.ac.id>
2023-01-06 16:52:16 +0700

    pertama

---
+++ aboutus.php
@@ -0,0 +1,380 @@
+
+<!DOCTYPE html>
+<html class="light">
+	<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>About Us</title>	
+
+		<meta name="keywords" content="WebSite Template" />
+		<meta name="description" content="Porto - Multipurpose Website Template">
+		<meta name="author" content="okler.net">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<!-- Web Fonts  -->
+		<link id="googleFonts" href="https://fonts.googleapis.com/css?family=family=Lora:400,400i,700,700i|Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+		<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+		<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css">
+		<link rel="stylesheet" href="css/theme-elements.css">
+		<link rel="stylesheet" href="css/theme-blog.css">
+		<link rel="stylesheet" href="css/theme-shop.css">
+
+		<!-- Demo CSS -->
+		<link rel="stylesheet" href="css/demos/demo-digital-agency-2.css">
+
+		<!-- Skin CSS -->
+		<link id="skinCSS" rel="stylesheet" href="css/skins/skin-digital-agency-2.css">
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="vendor/modernizr/modernizr.min.js"></script>
+
+	</head>
+	<body>
+
+		<div class="body">
+			<header id="header" class="header-transparent header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyChangeLogo': false, 'stickyStartAt': 1, 'stickyHeaderContainerHeight': 100}">
+				<div class="header-body border-top-0 bg-color-dark box-shadow-none">
+					<div class="header-container container">
+						<div class="header-row">
+							<div class="header-column header-column-logo">
+								<div class="header-row">
+									<div class="header-logo">
+										<a href="demo-digital-agency-2.html">
+                                        <img alt="Porto" width="120" height="70" src="img/demos/digital-agency-2/logos/Logo Sekunder.png">
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="header-column header-column-nav-menu justify-content-end w-100">
+								<div class="header-row">
+                                    <div class="header-nav header-nav-links header-nav-dropdowns-dark header-nav-dark-text order-2 order-lg-1">
+										<div class="header-nav-main header-nav-main-mobile-dark header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
+											<nav class="collapse">
+												<ul class="nav nav-pills" id="mainNav">
+													<li class="dropdown-primary">
+														<a class="nav-link text-capitalize font-weight-semibold custom-text-3" href="index.php">
+															Our House
+														</a>
+													</li>
+													<li class="dropdown-primary">
+														<a class="nav-link text-capitalize font-weight-semibold custom-text-3 active" href="aboutus.php">
+															About Us
+														</a>
+													</li>
+													<li class="dropdown">
+														<a class="nav-link dropdown-toggle text-capitalize font-weight-semibold custom-text-3" class="dropdown-toggle" href="ourworks.php">
+															Our Works
+														</a>
+														<ul class="dropdown-menu">
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services.html">
+																	Overview
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services-detail.html">
+																	UX Design
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services-detail.html">
+																	Web Development
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services-detail.html">
+																	B2B Software
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services-detail.html">
+																	Content Marketing
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services-detail.html">
+																	Email Marketing
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services-detail.html">
+																	Search Engine Optimize
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services-detail.html">
+																	Social Media Ads
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services-detail.html">
+																	Brand and Identity
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item font-weight-normal" href="demo-digital-agency-2-our-services-detail.html">
+																	Content Strategy
+																</a>
+															</li>
+														</ul>
+													</li>
+													
+
+													<li class="dropdown-primary">
+														<a class="nav-link text-capitalize font-weight-semibold custom-text-3" href="contactus.php">
+															Contact Us
+														</a>
+													</li>
+												</ul>
+											</nav>
+										</div>
+										<button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
+											<i class="fas fa-bars"></i>
+										</button>
+									</div>
+								</div>
+							</div>
+							<div class="header-column justify-content-end d-none d-lg-flex">
+								<div class="header-row">
+									<ul class="header-social-icons social-icons social-icons-clean social-icons-icon-dark social-icons-big d-lg-flex m-0 ms-lg-2">
+										<li class="social-icons-instagram"><a href="http://www.instagram.com/" target="_blank" class="text-4" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+										
+										<li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" class="text-4" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<div role="main" class="main">
+
+				<section class="page-header page-header-modern page-header-background bg-color-dark p-relative z-index-1 lazyload" data-bg-src="img/demos/digital-agency-2/bg/bg-baru.jpg">
+					<span class="custom-circle custom-circle-1 bg-color-light custom-circle-blur appear-animation" data-appear-animation="fadeInLeftShorter" data-appear-animation-delay="400"></span>
+					<span class="custom-circle custom-circle-2 bg-color-primary appear-animation" data-appear-animation="zoomIn" data-appear-animation-delay="500"></span>
+					<span class="custom-circle custom-circle-3 bg-color-primary appear-animation" data-appear-animation="zoomIn" data-appear-animation-delay="600"></span>
+					<div class="container">
+						<div class="row mt-5">
+							<div class="col">
+								<ul class="breadcrumb breadcrumb-light custom-title-with-icon-primary d-block">
+									<li><a href="index.php" class="text-color-dark font-weight-extra-bold">Home</a></li>
+									<li class="active text-color-dark font-weight-extra-bold">About Us</li>
+								</ul>
+								<h1 class="custom-text-10 font-weight-bold text-color-dark">About Us</h1>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<div class="container p-relative z-index-2 mt-0 mt-lg-3 mt-xl-0 pt-0 pt-md-4 pt-lg-5 pb-xl-5 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="200">
+                    
+                <article class="post post-large">
+									<div class="post-image">
+										<div class="ratio ratio-16x9">
+											<iframe  src="https://www.youtube.com/embed/UalTfOIDQ7M" width="640" height="360" allowfullscreen></iframe>
+										</div>
+									</div>
+
+									<div class="post-date">
+										<span class="day">7</span>
+										<span class="month">Jan</span>
+									</div>
+
+									<div class="post-content">
+
+										<h2 class="font-weight-semibold text-6 line-height-3 mb-3"><a href="blog-post.html">ini adalah video perkenalan perusahaan kami</a></h2>
+										<p>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Pellentesque pellentesque tempor tellus eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae. [...]</p>
+
+										
+
+									</div>
+								</article>
+				</div>
+
+				<section class="bg-color-grey mb-5 mt-lg-5">
+					<div class="container py-5">
+                    <article class="post post-large">
+									<div class="post-image">
+										<div class="owl-carousel owl-theme show-nav-hover dots-inside nav-inside nav-style-1 nav-light" data-plugin-options="{'items': 1, 'margin': 10, 'loop': false, 'nav': true, 'dots': true}">
+											<div>
+												<div class="img-thumbnail border-0 p-0 d-block">
+													<img class="img-fluid border-radius-0" src="img/blog/wide/profile01.png" alt="">
+												</div>
+											</div>
+											<div>
+												<div class="img-thumbnail border-0 p-0 d-block">
+													<img class="img-fluid border-radius-0" src="img/blog/wide/profile02.png" alt="">
+												</div>
+											</div>
+											<div>
+												<div class="img-thumbnail border-0 p-0 d-block">
+													<img class="img-fluid border-radius-0" src="img/blog/wide/profile03.png" alt="">
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="post-date">
+										<span class="day">9</span>
+										<span class="month">Jan</span>
+									</div>
+
+									<div class="post-content">
+
+										<h2 class="font-weight-semibold text-6 line-height-3 mb-3"><a href="blog-post.html">This is a stardard slider gallery post</a></h2>
+										<p>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Pellentesque pellentesque tempor tellus eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae. [...]</p>
+
+									</div>
+								</article>
+					</div>
+				</section>
+
+				
+				<section class="get-in-touch bg-color-after-secondary overlay  overlay-show p-relative overflow-hidden" style="background-image: url('img/demos/digital-agency-2/bg/bg-baru.jpg'); background-size: cover; background-position: center;">
+					<span class="custom-circle custom-circle-1 bg-color-light appear-animation" data-appear-animation="zoomIn" data-appear-animation-delay="100"></span>
+					<span class="custom-circle custom-circle-2 bg-color-light appear-animation" data-appear-animation="zoomIn" data-appear-animation-delay="100"></span>
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8">
+								<p class="mb-2 text-color-tertiary custom-text-7 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="200">Let’s Get in Touch</p>
+								<h4 class="text-color-light font-weight-bold custom-text-10 appear-animation" data-appear-animation="fadeInRightShorter" data-appear-animation-delay="400">
+									We’re interested in talking<br/>
+									about your business.
+								</h4>
+							</div>
+							<div class="col-lg-4 d-flex align-items-center justify-content-start justify-content-lg-end mt-5 mt-lg-0">
+								<a herf="#" class="btn btn-outline custom-btn-outline btn-light border-white rounded-0 px-4 py-3 text-color-light text-color-hover-dark bg-color-hover-light custom-text-6 line-height-6 font-weight-semibold custom-btn-with-arrow appear-animation" data-appear-animation="fadeInLeftShorter" data-appear-animation-delay="600">Let’s Talk!</a>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			</div>
+
+			<footer id="footer" class="mt-0 py-5">
+				<div class="container py-5">
+					<div class="row justify-content-between">
+						<div class="col-sm-12 col-lg-6 col-xl-6">
+							<div class="appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="100">
+								<h4 class="custom-font-primary custom-newsletter-title font-weight-bold mb-4 custom-text-7">Join for Company Updates</h4>
+								<div class="alert alert-success d-none" id="newsletterSuccess">
+									<strong>Success!</strong> You've been added to our email list.
+								</div>
+								<div class="alert alert-danger d-none" id="newsletterError"></div>
+								<form id="newsletterForm" action="php/newsletter-subscribe.php" method="POST" class="me-0 mb-3 mb-md-0 opacity-10">
+									<div class="input-group custom-newsletter">
+										<input class="form-control form-control-sm custom-newsletter-input rounded-0 bg-transparent border-0 ps-0 custom-text-2 text-color-light box-shadow-none" placeholder="Your E-mail Address" name="newsletterEmail" id="newsletterEmail" type="email">
+										<button class="btn text-color-light custom-text-4 font-weight-semibold custom-btn-with-arrow custom-btn-with-arrow-light" type="submit">Sign Up</button>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div class="col-sm-12 col-lg-6 col-xl-4 col-info-footer mt-4 mt-sm-5 mt-lg-0">
+							<div class="row justify-content-center">
+								<div class="col-md-6 d-flex flex-column justify-content-center align-items-center"> 
+									<span class="d-block text-start text-lg-end text-color-light font-weight-semibold text-5 pb-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="100">Jakarta Barat</span>
+									<p class="mb-0 text-start text-lg-end text-4 font-weight-medium appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="150">Ruko Mutiara Taman Palem Blok C 10 No.51, Cengkareng</p>
+									<p class="mb-0 text-start text-lg-end text-4 font-weight-medium appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">DKI Jakarta, 11730</p>
+									<p class="mb-0 text-start text-lg-end text-4 font-weight-medium appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="250"><a href="tel:+1234567890" class="text-color-default">(123) 456-7890</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row justify-content-between">
+						<div class="col-sm-12 col-lg-7 col-xl-6 d-none d-sm-flex">
+							<div class="nav-footer w-100 pt-5 mt-0 mt-lg-4">
+								<div class="row justify-content-between">
+									<div class="col-auto me-auto">
+										<div class="appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">
+											<div class="footer-nav footer-nav-links">
+												<nav>
+													<ul class="nav" id="footerNav">
+														<li>
+															<a class="text-color-hover-primary font-weight-semibold custom-text-2 text-capitalize" href="demo-digital-agency-2-our-services.html">Our Services</a>
+														</li>
+														<li>
+															<a class="text-color-hover-primary font-weight-semibold custom-text-2 text-capitalize" href="demo-digital-agency-2-our-work.html">Our Work</a>
+														</li>
+														<li>
+															<a class="text-color-hover-primary font-weight-semibold custom-text-2 text-capitalize" href="demo-digital-agency-2-contact-us.html">Contact Us</a>
+														</li>
+													</ul>
+												</nav>
+											</div>			
+										</div>							
+									</div>
+									<div class="col-auto">
+										<div class="appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="100">
+											<ul class="header-social-icons social-icons social-icons-clean social-icons-icon-light social-icons-big d-none d-lg-block m-0 p-relative bottom-10">
+												<li class="social-icons-instagram"><a href="http://www.instagram.com/" target="_blank" class="text-4" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+												<li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" class="text-4" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+												<li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" class="text-4" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-12 col-lg-5 col-xl-6">
+							<div class="d-flex justify-content-end custom-footer-copywriting pt-5 mt-0 mt-lg-4">
+								<p class="mb-0 text-start text-lg-end d-block w-100 appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="300">Porto Digital Agency. ©  2022.  All Rights Reserved</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+
+		<!-- Vendor -->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="vendor/lazysizes/lazysizes.min.js"></script>
+		<script src="vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="vendor/vide/jquery.vide.min.js"></script>
+		<script src="vendor/vivus/vivus.min.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="js/views/view.contact.js"></script>
+
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+	</body>
+</html>
